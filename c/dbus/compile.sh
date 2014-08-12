5d715e45a18fcbac5d5a6f6177eaff8e59ff554f
xifei <xifei@nfs.iscas.ac.cn>
2014-08-12 09:08:29 +0800

    modify compile for dbus

--- c/dbus/compile.sh
+++ c/dbus/compile.sh
@@ -4,4 +4,4 @@ if [ -z $1 ] ; then
     exit 1
 fi
 
-gcc `pkg-config --cflags glib-2.0 gthread-2.0 dbus-1` $1 -o binary `pkg-config --libs glib-2.0 gthread-2.0 dbus-1`
+gcc `pkg-config --cflags glib-2.0 gthread-2.0 dbus-1 dbus-glib-1` $1 -o binary `pkg-config --libs glib-2.0 gthread-2.0 dbus-1 dbus-glib-1`
