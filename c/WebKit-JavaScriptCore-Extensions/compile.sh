7951f78ad0fd3d75c97c1a8ecb707cb41ed4817e
xifei <xifei@nfs.iscas.ac.cn>
2014-08-15 15:39:14 +0800

    modify compile.sh in c/WebKit-JavaScriptCore-Extensions

--- c/WebKit-JavaScriptCore-Extensions/compile.sh
+++ c/WebKit-JavaScriptCore-Extensions/compile.sh
@@ -4,4 +4,4 @@ if [ -z $1 ] ; then
     exit 1
 fi
 
-gcc -g `pkg-config --cflags webkitgtk-3.0 cairo dbus-1 dbus-glib-1` $1 -o binary `pkg-config --libs webkitgtk-3.0 cairo dbus-1 dbus-glib-1`
+gcc -g `pkg-config --cflags webkitgtk-3.0 cairo dbus-1 dbus-glib-1 libnotify` $1 -o binary `pkg-config --libs webkitgtk-3.0 cairo dbus-1 dbus-glib-1 libnotify`
