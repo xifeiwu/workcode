86320cb23900ab3ac3211b79df07e67177e4eacc
xifei <xifei@nfs.iscas.ac.cn>
2014-08-23 21:49:50 +0800

    avahi-mdns example: test_client.py

--- c/dbus/seminar/dbus-call/org.freedesktop.Avahi/test_client.py
+++ c/dbus/seminar/dbus-call/org.freedesktop.Avahi/test_client.py
@@ -17,7 +17,7 @@ def print_error(*args):
     print 'error_handler'
     print args[0]
 
-def myhandler(interface, protocol, name, stype, domain, flags):
+def ItemAdd(interface, protocol, name, stype, domain, flags):
     print "Found service '%s' type '%s' domain '%s' " % (name, stype, domain)
 
     if flags & avahi.LOOKUP_RESULT_LOCAL:
@@ -28,22 +28,29 @@ def myhandler(interface, protocol, name, stype, domain, flags):
         domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
         reply_handler=service_resolved, error_handler=print_error)
 
+def ItemRemove(interface, protocol, name, stype, domain, flags):
+    print "service '%s' type '%s' domain '%s' Remove" % (name, stype, domain)
+    print "Addation: interface '%r' protocol '%r' flags '%r'" % (interface, protocol, flags)
+
 loop = DBusGMainLoop()
 
 bus = dbus.SystemBus(mainloop=loop)
 
+server = dbus.Interface( bus.get_object(avahi.DBUS_NAME, '/'),
+        'org.freedesktop.Avahi.Server')
+print "parameters in ServiceBrowserNew:%d, %d, %s, %s, %d" % (avahi.IF_UNSPEC, avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))
+service_browser_path = server.ServiceBrowserNew(avahi.IF_UNSPEC,
+            avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))
+sbrowser = dbus.Interface(bus.get_object(avahi.DBUS_NAME,service_browser_path),
+        avahi.DBUS_INTERFACE_SERVICE_BROWSER)
+
 print "avahi.DBUS_NAME: %s" % (avahi.DBUS_NAME)
 print "avahi.DBUS_PATH_SERVER: %s" % (avahi.DBUS_PATH_SERVER)
 print "avahi.DBUS_INTERFACE_SERVER: %s" % (avahi.DBUS_INTERFACE_SERVER)
+print "Service Browser Path: %s" % (service_browser_path)
+print "avahi.DBUS_INTERFACE_SERVICE_BROWSER: %s" % (avahi.DBUS_INTERFACE_SERVICE_BROWSER)
 
-server = dbus.Interface( bus.get_object(avahi.DBUS_NAME, '/'),
-        'org.freedesktop.Avahi.Server')
-
-sbrowser = dbus.Interface(bus.get_object(avahi.DBUS_NAME,
-        server.ServiceBrowserNew(avahi.IF_UNSPEC,
-            avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))),
-        avahi.DBUS_INTERFACE_SERVICE_BROWSER)
-
-sbrowser.connect_to_signal("ItemNew", myhandler)
+sbrowser.connect_to_signal("ItemNew", ItemAdd)
+sbrowser.connect_to_signal("ItemRemove", ItemRemove)
 
 gobject.MainLoop().run()
