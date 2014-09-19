f88b5f485f8fea24fd2dbfe2a1eb29c5f942d16f
xifei <xifei@nfs.iscas.ac.cn>
2014-09-19 10:47:42 +0800

    add new dir in html5/js

---
+++ html5/js/null/testnull.js
@@ -0,0 +1,10 @@
+function returnobj(){
+    obj = new Object();
+//    obj = null;
+    return obj;
+}
+if(returnobj()){
+   console.log("obj is an object.");
+}else{
+   console.log("obj is null.");
+}
