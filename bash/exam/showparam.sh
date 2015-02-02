a5e27003e2210fe670ac750c753ffeebbe60139c
xifei <xifei@nfs.iscas.ac.cn>
2015-02-02 22:54:39 +0800

    bash shell

--- bash/exam/showparam.sh
+++ bash/exam/showparam.sh
@@ -1,6 +1,15 @@
 echo "number of parameters:$#"
-echo "\$@:$@"
-echo "\$*:$*"
+argc=$#
+argv=($@)
+echo $argc ${argv[@]}
+for((i=0;i<=argc;i++))
+do
+    echo $i:${argv[$i]}
+done
+
+echo ===============================
+echo $@
+echo $*
 echo "zero:$0"
 echo "one:$1"
 echo "two:$2"
--- bash/operfile/findWord.sh
+++ bash/operfile/findWord.sh
@@ -1,8 +1,25 @@
-for file in  `find . -name "*.js"`
+argc=$#
+argv=($@)
+if [ $argc -lt 2 ]; then
+    echo Parameter Less Than Two.
+    echo First is file type, Others is key word.
+    exit 1
+fi
+
+count=2
+grepkey=""
+for((i=1;i<argc;i++))
 do
-    cat $file | grep "require" | grep "commonHandle"
+    grepkey="${grepkey} grep ${argv[$i]}"
+done
+
+echo find . -name \"${argv[0]}\"
+echo $grepkey
+
+for file in  `find . -name ${argv[0]}`
+do
+    cat $file | $grepkey
     if [ $? -eq 0 ]; then
-        echo Found in file: $file;
+        echo -e "\033[2;31mFound in file: $file\033[0m"
     fi
 done
-#| grep "require" | grep "device"
