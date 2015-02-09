2d8eee2752707c557ee6d1bb8743c2bb10bdc8d7
xifei <xifei@nfs.iscas.ac.cn>
2015-02-09 14:19:50 +0800

    nodejs server structure.

--- node/nodejs/server/start.js
+++ node/nodejs/server/start.js
@@ -1,6 +1,6 @@
-var server = require("./server2");
-var router = require("./router2");
-var requestHandlers = require("./requestHandlers2");
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 handle["/"] = requestHandlers.start;
