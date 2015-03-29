eaec9b3e2890ac0e7c5b28703fca38928b5e3479
xifei <xifei@nfs.iscas.ac.cn>
2015-03-29 11:09:36 +0800

    test Array.prototype.slice.call method

--- node/nodejs/Array/testSlice.js
+++ node/nodejs/Array/testSlice.js
@@ -2,5 +2,7 @@ function slice(){
     result = Array.prototype.slice.call(arguments);
     console.log("arguments: ", arguments);
     console.log("result: ", result);
+    console.log("json result: ", JSON.stringify(result));
 }
-slice([1,2,3,4,5,67,87,99]);
+//slice(1,2,3,4,5,67,87,99);
+slice(true, {"title": "Title"});
