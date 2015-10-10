eaaeab8b8e8a2e4ad42371146e794410472c5c5d
xifei.wu <xifei.wu@acadine.com>
2015-10-10 14:28:27 +0800

    grammer study.

--- html5/js/BOM/Grammer/define_property.js
+++ html5/js/BOM/Grammer/define_property.js
@@ -17,4 +17,26 @@
       return _launchContext;
     }
   });
+
+  var book = {
+      _year: 2004,
+      edition: 1
+  };
+  Object.defineProperty(book, "year", {
+      get: function(){
+          return this._year;
+      },
+      set: function(newValue){
+      
+          if (newValue > 2004) {
+              this._year = newValue;
+              this.edition += newValue - 2004;
+          
+          }
+      }
+  });
+  //book.year = 2005;
+  console.log(book.edition);   //2
+  exports.book = book;
+
 })(window);
--- html5/js/BOM/Grammer/use_strict.js
+++ html5/js/BOM/Grammer/use_strict.js
@@ -1,8 +1,23 @@
 "use strict";
-try {
-  // 假如有一个全局变量叫做mistypedVariable
-  mistypedVaraible = 17; // 因为变量名拼写错误
-  // 这一行代码就会抛出 ReferenceError
-} catch (e) {
-  console.log(e);
-}
+(function(exports){
+  try {
+    // 假如有一个全局变量叫做mistypedVariable
+    var mistypedVaraible = 17; // 因为变量名拼写错误
+    // 这一行代码就会抛出 ReferenceError
+  } catch (e) {
+    console.log(e);
+  }
+  
+  var run = function(fromWhom){
+      arguments[0] = 'alien';
+      console.log(fromWhom);
+  }
+  run('zombie');
+
+  var person = {};
+  Object.defineProperty(person, "name", {
+    writable: false,
+    value: "Nicholas"
+  });
+  exports.person = person;
+})(window);
