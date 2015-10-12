872a095d8afe57496883dfa7708b78e7ef8b225d
xifei.wu <xifei.wu@acadine.com>
2015-10-12 16:08:34 +0800

    fix grammer.html

--- html5/javascript/Grammer/prototype/__proto__.js
+++ html5/javascript/Grammer/prototype/__proto__.js
@@ -1,8 +1,12 @@
 (function(exports){
   var Person = function(){
   }
-  var person = new Person();
-  if(person.__proto__ == Person.prototype) {
+  var person1 = new Person();
+  if(person1.__proto__ == Person.prototype) {
     console.log("The same");
   }
+  var person2 = function(){};
+  person2.prototype = Person; 
+  window.person1 = person1;
+  window.person2 = person2;
 })(window)
